67502f4ef8fd0d3e249d4273a18a434e284a6c99
Ashmit Mukherjee <73027247+Ansester@users.noreply.github.com>
2025-02-10 16:05:10 +0000

    my new app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,34 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Data Science App")
+st.write("Hello how are you doing. Welcome to my app")
+
+st.sidebar.title("Select Dataset")
+
+st.image("cat.jpg")
+
+
+##as Data Visualization
+
+app_mode = st.sidebar.selectbox("Select a page",["01 Introduction","02 Data Visualization"])
+df = pd.read_csv("dataset.csv")
+
+if app_mode == "01 Introduction":
+    st.write("Let's start exploring the dataset")
+    df=pd.read_csv("dataset.csv")
+
+    st.dataframe(df.head(5))
+
+    st.markdown("### Statistics summary of the dataset")
+
+
+
+
+    st.dataframe(df.describe())
+elif app_mode == "02 Data Visualization":
+    st.write("Hello World")
+    df=pd.read_csv("dataset.csv")
+    sns.bar_chart("")
